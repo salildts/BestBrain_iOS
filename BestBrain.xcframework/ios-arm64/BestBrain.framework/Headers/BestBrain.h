840cb7ba9640947a17f547d4ef661ab601161580
Mithun Bera <mithun.bera@dreamztech.com>
2024-09-12 19:33:50 +0530

    Initial commit

---
+++ BestBrain.xcframework/ios-arm64/BestBrain.framework/Headers/BestBrain.h
@@ -0,0 +1,18 @@
+//
+//  BestBrain.h
+//  BestBrain
+//
+//  Created by Mithun Bera on 12/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BestBrain.
+FOUNDATION_EXPORT double BestBrainVersionNumber;
+
+//! Project version string for BestBrain.
+FOUNDATION_EXPORT const unsigned char BestBrainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BestBrain/PublicHeader.h>
+
+
---
+++ BestBrainExample/BestBrainExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestBrainExample
+//
+//  Created by Mithun Bera on 12/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
