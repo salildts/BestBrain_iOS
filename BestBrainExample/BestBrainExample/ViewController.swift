0510b5e9e682fd4d588217fc06b57bc716f8e817
Mithun Bera <mithun.bera@dreamztech.com>
2024-09-12 20:05:19 +0530

    example

--- BestBrainExample/BestBrainExample/ViewController.swift
+++ BestBrainExample/BestBrainExample/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import BestBrain
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        BestBrainConfig.setConfig(baseurl: "<https://YOUR-SERVER/api/v1/>", socketUrl: "<YOUR_SOCKET_SERVER>", apiKey: "<YOUR_API_KEY>", appId: "<YOUR_APP_ID>")
     }
 
-
+    @IBAction func openChatboat(_ sender: Any) {
+        let chatVC = BestBrainMessageViewController.getMsgVc()
+        self.navigationController?.pushViewController(chatVC, animated: true)
+    }
+    
 }
 
